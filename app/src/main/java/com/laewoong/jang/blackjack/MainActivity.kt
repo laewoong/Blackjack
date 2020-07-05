b82db9566cbd80a88804639fdec2e4958d5b5d95
laewoong <simpler23@hanmail.net>
2020-07-05 14:33:00 +0900

    Initial commit

---
+++ app/src/main/java/com/laewoong/jang/blackjack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.laewoong.jang.blackjack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
